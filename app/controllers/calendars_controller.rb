8a9dbc5df010d020c5d35eca8916704ea318edab
Francisco Renteria <renteriav@hotmail.com>
2013-05-28 22:07:41 -0700

    finished calendar controller and view

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -141,6 +141,116 @@ class CalendarsController < ApplicationController
       t += 1
     end
     
+#  makes an array of index for each event. The index is the position in which 
+#  an event is placed in the calendar from left to right.
+   
+    @index = Array.new
+    @index_array = Array.new
+    @rooms.each do |room|
+      @index_array[room.id] = Hash.new
+    end
+    d = 0
+    while d < @num_rooms do
+      if @slot[d] != nil
+        sl = 0
+        while sl < @num_rows do
+          @keys = @slot[d][sl].count
+          @prev = @slot[d][sl - 1]
+          @index.clear
+          if @prev
+            i = 0
+            while i < @keys do
+              if @prev.include?(@slot[d][sl][i]) 
+                @index.push(@index_array[d][@slot[d][sl][i]])
+              else
+                if @index.empty?
+                  @counter = 0
+                  @slot[d][sl].each do |event|
+                    @index_array[d].merge!(event => @counter)
+                    @counter += 1
+                  end
+                else
+                  @index.sort! {|x,y| x <=> y }
+                  @counter = 0
+                  while @index[@counter] == @counter do
+                    @counter += 1
+                  end
+                  @index_array[d][@slot[d][sl][i]] = @counter
+                  @index.push(@counter) 
+                end
+              end
+              i += 1
+            end
+          else
+            @counter = 0
+            if @slot[d][sl] != nil
+              @slot[d][sl].each do |event|
+                @index_array[d].merge!(event => @counter)
+                @counter += 1
+              end
+            end
+          end
+          sl += 1
+        end
+      end
+      d += 1
+    end
+    
+#  makes an array with the ids and indexes of lessons per slot ordered by index
+    
+    @event_index = Array.new
+    @rooms.each do |room|
+      @event_index[room.id] = Array.new(@num_rows){Array.new}
+    end
+    
+    d = 0
+    while d < @num_rooms do
+      i = 0
+      if @slot[d] != nil
+        while i < @num_rows do
+          if @slot[d][i] != nil 
+            @slot[d][i].each do |id|
+              @event_index[d][i].push({:index=>@index_array[d][id], :id => id})
+              @event_index[d][i].sort! {|a,b| a[:index] <=> b[:index]}
+            end
+          end
+          i += 1
+        end
+      end
+      d += 1
+    end
+    
+# Calculates when an event needs more than 1 space.
+    @num_slots = Hash.new
+    d = 0
+    while d < @num_rows
+    	if @columns[d] != nil
+    	  @columns[d].each do |column|
+    			@num_slots.merge!(column[:id] => @max_num[d][column[:id]] - @index_array[d][column[:id]])
+        end
+        sl = 0
+    		while sl < @num_rows do
+    			if @event_index[d][sl] != nil
+    				@num_events = @event_index[d][sl].count
+            i = 0
+    				while i < (@num_events - 1) do
+    					@ind = @event_index[d][sl][i][:index]
+    					@next_ind = @event_index[d][sl][i+1][:index]
+    					@event_id = @event_index[d][sl][i][:id]
+    					@result = @next_ind - @ind
+    					if @num_slots[@event_id] != nil
+                if @num_slots[@event_id] > @result
+    						  @num_slots[@event_id] = @result
+                end
+              end
+              i += 1
+            end
+          end
+    		sl += 1
+        end
+      end
+      d += 1
+    end    
   end
 end
 
