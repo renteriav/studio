42a6be6810bddd4d230b7fb2677054b93f8a7cc4
Francisco Renteria <renteriav@hotmail.com>
2014-03-30 16:59:06 -0700

    assets compile true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,12 +15,12 @@ Studio::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
   
-  config.whiny_nils = true
+  config.whiny_nils = false
   
   config.eager_load = true
 
